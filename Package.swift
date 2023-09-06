8fe461a87eb15f6d0ada0bb4d7fbb48cefb7cb53
marcprux <mwp1@cornell.edu>
2023-09-06 15:28:20 -0400

    Move XCTest utilities over to skip

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://source.skip.tools/skip.git", from: "0.6.43"),
     ],
     targets: [
-        .target(name: "SkipUnit", dependencies: [.product(name: "SkipDrive", package: "skip")], plugins: [.plugin(name: "skipstone", package: "skip")]),
-        .testTarget(name: "SkipUnitTests", dependencies: ["SkipUnit"], plugins: [.plugin(name: "skipstone", package: "skip")]),
+        .target(name: "SkipUnit", plugins: [.plugin(name: "skipstone", package: "skip")]),
+        .testTarget(name: "SkipUnitTests", dependencies: ["SkipUnit", .product(name: "SkipTest", package: "skip")], plugins: [.plugin(name: "skipstone", package: "skip")]),
     ]
 )
