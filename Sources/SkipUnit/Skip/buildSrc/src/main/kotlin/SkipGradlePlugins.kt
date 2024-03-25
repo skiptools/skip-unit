fb9ed465611f4aae7b19ebbe379dba2e66775e67
marcprux <mwp1@cornell.edu>
2024-03-25 10:37:51 -0400

    Add license headers

--- Sources/SkipUnit/Skip/buildSrc/src/main/kotlin/SkipGradlePlugins.kt
+++ Sources/SkipUnit/Skip/buildSrc/src/main/kotlin/SkipGradlePlugins.kt
@@ -1,3 +1,7 @@
+// This is free software: you can redistribute and/or modify it
+// under the terms of the GNU Lesser General Public License 3.0
+// as published by the Free Software Foundation https://fsf.org
+
 import org.gradle.api.*
 import org.gradle.api.initialization.*
 import org.gradle.api.provider.Property
