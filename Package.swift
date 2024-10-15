025d7240090310caeb5a3bef68ff61e0576ea605
Marc Prud'hommeaux <marc@skip.tools>
2024-10-15 18:44:05 -0400

    Move Robolectric test dependencies from skip-foundation to skip-unit

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "SkipUnit", targets: ["SkipUnit"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "1.0.0"),
+        .package(url: "https://source.skip.tools/skip.git", from: "1.1.11"),
     ],
     targets: [
         .target(name: "SkipUnit", plugins: [.plugin(name: "skipstone", package: "skip")]),
