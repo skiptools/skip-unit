3daba4dd719a7d8815535e514d7472054ed20866
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-08-12 06:37:25 -0400

    Linux support

--- Package.swift
+++ Package.swift
@@ -8,8 +8,7 @@ let package = Package(
         .library(name: "SkipUnit", targets: ["SkipUnit"]),
     ],
     dependencies: [
-        //.package(url: "https://source.skip.tools/skip.git", from: "1.6.0"),
-        .package(path: "../skip"),
+        .package(url: "https://source.skip.tools/skip.git", branch: "linux"),
     ],
     targets: [
         .target(name: "SkipUnit", plugins: [.plugin(name: "skipstone", package: "skip")]),
