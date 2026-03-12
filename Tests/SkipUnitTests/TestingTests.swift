10db282b589a7ad10c83810df71fb7d00b8cb215
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-03-12 11:55:20 -0400

    Support for Swift Testing

---
+++ Tests/SkipUnitTests/TestingTests.swift
@@ -0,0 +1,18 @@
+// Copyright 2023–2026 Skip
+// SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
+import Testing
+
+@Test func freestandingTests() {
+    #expect("free" != "standing")
+}
+
+@Suite class TestingTests {
+    @Test func testTestingTests() {
+        #expect(1 == 1)
+        #expect(1 != 1 + 1)
+        #expect(1 < 2)
+        #expect(2 <= 2)
+        #expect(2 >= 2)
+        #expect(3 > 2)
+    }
+}
